0b77482a64487fe277584540d6923be38d7b75a9
imiles <ieshia.miles@gmail.com>
2025-12-02 17:33:52 -0500

    Added SQL schema for client and service_record

---
+++ Schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE client (
+    client_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    phone_num TEXT,
+    street_address TEXT,
+    city TEXT NOT NULL,
+    state_code TEXT NOT NULL,
+    email TEXT UNIQUE,
+    date_of_birth DATE
+
+);
+
+CREATE TABLE service_record (
+    record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_id INTEGER NOT NULL,
+    service_type TEXT NOT NULL,
+    date_received DATE NOT NULL,
+    notes TEXT,
+    FOREIGN KEY (client_id) REFERENCES client(client_id)
+);
