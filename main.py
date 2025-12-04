519294dd0795be607acb2dacca7e1c836d6cdbab
imiles <ieshia.miles@gmail.com>
2025-12-04 00:44:18 -0500

    Added post functions for clients and service records

--- main.py
+++ main.py
@@ -46,7 +46,6 @@ def get_service_record(record_id: int):
     return{"Service Record":results}
 
 class ClientCreate(BaseModel):
-    client_id: str
     first_name: str
     last_name: str
     phone_num: str
@@ -57,9 +56,47 @@ class ClientCreate(BaseModel):
     date_of_birth: str
 
 class Service_RecordCreate(BaseModel):
-    record_id: int
     client_id: int
     service_type: str
     date_received: str
     notes: str
-    
+
+class ClientUpdate(BaseModel):
+    first_name: str
+    last_name: str
+    phone_num: str
+    street_address: str
+    city: str
+    state_code: str
+    email: str
+
+class Service_RecordUpdate(BaseModel):
+    client_id: int
+    service_type: str
+    date_received: str
+    notes: str
+
+@app.post("/clients")
+def add_client(client: ClientCreate):
+    connection=sqlite3.connect("cityteam.db")
+    cursor=connection.cursor()
+    cursor.execute(
+        "Insert into clients(first_name, last_name, phone_num, street_address, city, state_code, email, date_of_birth) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
+        (client.first_name, client.last_name, client.phone_num, client.street_address, client.city, client.state_code, client.email, client.date_of_birth)
+    )
+    connection.commit()
+    connection.close()
+    return("Client successfully added")
+
+@app.post("/service_records")
+def add_service_record(service_record: Service_RecordCreate):
+    connection=sqlite3.connect("cityteam.db")
+    cursor=connection.cursor()
+    cursor.execute(
+        "Insert into service_records(client_id, service_type, date_received, notes) VALUES(?, ?, ?, ?)",
+        (service_record.client_id, service_record.service_type, service_record.date_received, service_record.notes)
+    )
+    connection.commit()
+    connection.close()
+    return("Service Record successfully added")
+
